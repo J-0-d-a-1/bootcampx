d34565d775a100d67a2f37d5f0a17035d9347cbc
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-03-19 21:19:07 -0700

    Change: changed to useing parametaerized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,21 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `
+const queryString = `
   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
   FROM teachers
   JOIN assistance_requests on teachers.id = teacher_id
   JOIN students on students.id = assistance_requests.student_id
   JOIN cohorts on cohorts.id = students.cohort_id
-  WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+  WHERE cohorts.name = $1
   GROUP BY teacher, cohort;
-  `
-  )
+  `;
+
+const cohortName = process.argv[2] || "JUL02";
+const values = [`${cohortName}`];
+
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((row) => {
       console.log(`${row.cohort}: ${row.teacher}`);
